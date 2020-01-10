1f49fc974b3ca6d1591c927ba97bd82d4e79ca25
shicks1080 <stuart.hicks@gmail.com>
2020-01-10 13:09:55 +0100

    Create actual place route and user route

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const placesRoutes = require('./routes/route-places')
+const usersRoutes = require('./routes/route-users')
 
 const app = express()
 
-app.use(placesRoutes)
+app.use('/api/places', placesRoutes)
+app.use('/api/user', usersRoutes)
 
 app.listen(5000)
--- routes/route-places.js
+++ routes/route-places.js
@@ -2,11 +2,27 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    console.log('GET request in places')
-    res.json({
-        message: 'It\'s Alive!'
+//This is temporary - will be replaced by the database
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'Scene of a heinous murder!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId
     })
+    res.json({ place })
 })
 
 module.exports = router
