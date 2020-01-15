2f6d6b2ba6100a0977dfca5e16da945250b35053
shicks1080 <stuart.hicks@gmail.com>
2020-01-15 15:44:45 +0100

    Connecting the Place and User models
    Allows the writing of IDs from one to the other and vice versa.

--- models/place.js
+++ models/place.js
@@ -11,7 +11,7 @@ const placeSchema = new Schema({
     lng: { type: Number, required: true }
   },
   address: { type: String, required: true },
-  creator: { type: String, required: true }
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }
 });
 
 module.exports = mongoose.model('Place', placeSchema)
