3f77d867f3ae8690ba476997168be51764031637
shicks1080 <stuart.hicks@gmail.com>
2020-01-15 15:54:26 +0100

    Optional example using populate
    Using populate to get a User's places from the User part of the database rather than the Places part of the database.

--- controllers/controller-places.js
+++ controllers/controller-places.js
@@ -31,21 +31,29 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  let places
-
+  
+  //let places
+  let userWithPlaces
   try {
-    places = await Place.find( {creator: userId })
+    // places = await Place.find({ creator: userId })
+    userWithPlaces = await User.findById(userId).populate('places')
   } catch (err) {
     const error = new HttpError('Something went wrong we couldn\'t find that user\'s places', 500)
     return next(error)
   }
 
-  if (!places || places.length === 0) {
+  //if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(new HttpError("Could not find a place for that user id", 404));
   }
 
   // Getters true converts the _id to id with type of string rather than object
-  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  // res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.json({
+    places: userWithPlaces.places.map(place =>
+      place.toObject({ getters: true })
+    )
+  });
 };
 
 const createPlace = async (req, res, next) => {
