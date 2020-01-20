b5f3672ee70d9388eb9836da9fd0ff820a699340
shicks1080 <stuart.hicks@gmail.com>
2020-01-20 14:45:15 +0100

    Adding multer for processing image uploading
    Adding multer to the project.
    Configuring it as middleware for user sign up.
    Correcting a typo in the app.js.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.setHeader('Access-Control-Allow_Methods', 'GET, POST, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
   next()
 })
 
---
+++ middleware/file-upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer')
+const uuid = require('uuid/v1')
+
+
+const MIME_TYPE_MAP = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpeg',
+    'image/png': 'png',
+}
+
+const fileUpload = multer({
+    limits: 500000,
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, 'uploads/images')
+        },
+        filename: (req, file, cb) => {
+            const ext = !!MIME_TYPE_MAP[file.mimetype]
+            cb(null, uuid() + '.' + ext)
+        }
+    }),
+    fileFilter: (req, file, cb) => {
+        // !! means that if this is undefined or null false gets returned
+        // otherwise true
+        const isValid = !!MIME_TYPE_MAP[file.mimetype]
+        let error = isValid ? null : new Error('Invalid mime type.')
+        cb(error, isValid)
+    }
+})
+
+module.exports = fileUpload
