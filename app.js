0bfb66c096a1b0a05759065cd10a9b5641a30880
shicks1080 <stuart.hicks@gmail.com>
2020-01-20 15:26:04 +0100

    Making uploaded images accessible

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const path = require('path')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
@@ -11,6 +13,8 @@ const HttpError = require('./models/http-error')
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
