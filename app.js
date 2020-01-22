dc55fea7c435a47f129b496c2d616f4336256813
shicks1080 <stuart.hicks@gmail.com>
2020-01-22 15:16:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
   next()
 })
 
@@ -58,7 +58,7 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(error => {
     console.log(error);
